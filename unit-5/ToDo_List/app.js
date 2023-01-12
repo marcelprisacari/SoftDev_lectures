455e0ec2591d7a0c389e93b1d5039b6c8b54daf7
Marcel Prisacari <marcel.prisacari.com>
2023-01-11 20:03:10 -0500

    addedd new controller

--- unit-5/ToDo_List/app.js
+++ unit-5/ToDo_List/app.js
@@ -2,6 +2,12 @@ const express = require("express"); // being pulled from node modules.
 const app = express(); // simplifyig method calls.
 const PORT = 4000; // server port within localhost. Using 'all caps' to indicate a general variable.
 const practiceController = require("./controllers/practice.controller");
+const authController = require("./controllers/auth.controller");
+const { logTime } = require("./helpers");
+
+app.use(logTime); // using our own middleware function
+app.use(express.json()); // Provides us access to use JSON within our node.
+app.use(express.urlencoded); // parses the body from our browser so it can display the response.
 
 app.use(express.static(`${__dirname}/public`)); //__dirname comes from node.
 console.log("pathway: ", __dirname); //routes us to the root folder of our project
@@ -11,5 +17,6 @@ app.use("/practice", practiceController);
     Any traffic coming in, that has "practice" after localhost:4000, will route to practice.controller.js
         ex: localhost:4000/practice.
 */
+app.use("/todo", authController);
 
 app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
