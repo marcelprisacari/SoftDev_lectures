93725d580d177f9cb974f15e29aa000bdbf69dcb
Marcel Prisacari <marcel.prisacari.com>
2023-01-18 21:09:52 -0500

    update/delete routes

--- unit-5/ToDo_List/controllers/routes.controller.js
+++ unit-5/ToDo_List/controllers/routes.controller.js
@@ -1,103 +1,109 @@
-const router = require("express").Router();
-const db = require("../helpers/db.json");
-const fs = require("fs"); // library pulled from node. Allows to manipulate files.
+const router = require('express').Router();
+const db = require('../helpers/db.json');
+const fs = require('fs'); // library pulled from Node. Allows us to manipulate files.
 
-// TODO Get One
 /* 
     syntax:
         URL - /:id
             - denotes a parameter value. Endpoint is expecting a reference value.
-        Example URL
-            - localhost:4000/routes/1
+
+    Example URL    
+        - localhost:4000/routes/1
         
-            - "1" would reference a parameter or fexible string.
-                - This could be a name of something or anything that we want.
-                - depending on how the logic is built. 
+    - "1" would reference a parameter or flexible string.
+        - This could be a name of somethign or anything that we want.
+        - depending on how the logic is built.
+
 */
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
     // console.log(req.url);
-    // console.log(req.params); // details an object with a key of "id" (param name) & the value within the URL.
-    console.log(req.params.id);
+    // console.log(req.params); // details an object with a key of "id" (param name) & the value within the URL
+    // console.log(req.params.id);
 
     /* 
         - used to help us locate one item in our database
         - ID typically has a unique value to help us find that ONE item
-        - can help us search several items.
+        - Can help us search several items.
     */
+
     try {
+        
         let { id } = req.params;
-        let results = db.filter((i) => i.id == id);
+        let results = db.filter(i => i.id == id);
         // console.log(results);
-        // console.log(potato);
+        // console.log(potato) // for testing the break
         res.status(200).json({
             status: `Found item at id: ${id}`,
-            results,
-        });
+            results
+        })
+
     } catch (err) {
         res.status(500).json({
-            error: err.message,
-        });
+            error: err.message
+        })
     }
 });
 
-// TODO Get All
 /* 
     - Make a GET route
-        - no endpoint is required to access.
-        - it will "Get All" based off /routes.
+        - no endpoint is required to access. 
+        - It will "Get All" base off /routes.
             - note: where is "/routes" coming from?
 */
-router.get("/", (req, res) => {
-    // ex: localhost:4000/routes/
+router.get('/', (req, res) => {
+    //ex: localhost:4000/routes/
+
     try {
         res.status(200).json({
-            results: db,
-        });
+            results: db
+        })
+    
     } catch (err) {
         res.status(500).json({
-            error: err,
-        });
+            error: err.message
+        })
     }
 });
 
-// TODO POST a task
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
     try {
+        
         const toDoItem = req.body;
         // console.log(toDoItem);
 
-        fs.readFile("./helpers/db.json", (err, data) => {
-            // 1. The path needs to be relative to where fs is located (node_modules), NOT from the controllers location.
+        fs.readFile('./helpers/db.json', (err, data) => {
+            // 1. The path need to be relative to where fs is located (node_modules), NOT from the controllers location.
 
-            if (err) throw err; // conditional to deal with any error it comes across.
+            if(err) throw err; // conditional to deal with any error it comes across.
 
-            const db = JSON.parse(data); //parses the JSON file and provides an array of our objects back so we can manipulate it.
+            const db = JSON.parse(data); // parses the JSON file and provides an array of our objects back so we can manipulate it.
             // console.log(db);
 
-            db.push(toDoItem); // adding our new item to our array (db.json)
-            fs.writeFile("./helpers/db.json", JSON.stringify(db), (err) =>
-                console.log(err)
-            );
+            db.push(toDoItem); // adding our new item to our array.
+
+            fs.writeFile('./helpers/db.json', JSON.stringify(db), err => console.log(err));
 
             /* 
-            - Requires
-                - route to original file (same patway as readfile)
-                - what needs to be included or written
-                    - we are making our array BACK into a JSON format and stringify it.
-                - Callback function to handle erros.
+                - Requires
+                    - route to original file (same pathway as readfile)
+                    - what needs ot be included or written
+                        - We are making our array BACK into a JSON format and stringify-ing it.
+                    - Callback function to handle errors.
             */
 
             res.status(200).json({
                 status: "New Item Created!",
-                toDoItem,
+                toDoItem
             });
-        });
+
+        })
+
     } catch (err) {
         res.status(500).json({
-            error: err.message,
-        });
+            error: err.message
+        })
     }
 });
 
@@ -113,6 +119,52 @@ router.post("/", (req, res) => {
     * stretch goal: update just the data without modifying the ID
 */
 
+router.put('/:id', (req, res) => {
+
+    // console.log(req.params.id);
+    try {
+        
+        let request = Number(req.params.id);
+        // console.log(typeof request);
+
+        let todo = req.body;
+        // console.log(todo);
+
+        fs.readFile('./helpers/db.json', (err, data) => {
+            if(err) throw err;
+
+            // console.log(data.toString());
+            const database = JSON.parse(data);
+            // console.log(database instanceof Array);
+
+            let result;
+
+            database.forEach((obj, i) => {
+                // console.log(obj.id, i);
+
+                if(obj.id === request) {
+                    database[i] = todo;
+                    result = todo;
+                }
+
+                fs.writeFile('./helpers/db.json', JSON.stringify(database), err => console.log(err));
+            });
+
+            result ? 
+                res.status(200).json({
+                    object: result
+                }) :
+                res.status(404).json({ status: `ID: ${request} not found`});
+        })
+
+    } catch (err) {
+        res.status(500).json({
+            status: `Error: ${err.message}`
+        })
+    }
+
+});
+
 // TODO: Delete a task
 /* 
     - pass ID as a param
@@ -123,5 +175,37 @@ router.post("/", (req, res) => {
 
     *HINT: You may get stuck with your params not matching what's inside your id. Check your data type.
 */
+
+router.delete('/:id', (req, res) => {
+
+    
+    const request = Number(req.params.id);
+
+    fs.readFile('./helpers/db.json', (err, data) => {
+        if(err) throw err;
+
+        const db = JSON.parse(data);
+        let result = []
+        // console.log('Before ForEach: ', db);
+        db.forEach((obj, i) => {
+            if(obj.id !== request) {
+                result.push(db[i]);
+            }
+        });
+        // console.log('After: ', db);
+        // console.log(result);
+
+        fs.writeFile('./helpers/db.json', JSON.stringify(result), err => console.log(err));
+
+        result.length > 0 ? 
+            res.status(200).json({
+                object: result
+            }) :
+            res.status(404).json({
+                message: `ID: ${request} not found.`
+            })
+    })
+
+});
 
 module.exports = router;
