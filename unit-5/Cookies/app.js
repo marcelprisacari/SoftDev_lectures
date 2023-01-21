1b47bcec6bc4908914069c34db487f126ce0bab4
Marcel Prisacari <marcel.prisacari.com>
2023-01-21 15:07:42 -0500

    fixed typo

--- unit-5/Cookies/app.js
+++ unit-5/Cookies/app.js
@@ -3,10 +3,10 @@ const app = express();
 const PORT = 4003;
 const log = console.log;
 const cookieParser = require("cookie-parser");
-const cookiesController = require('./controllers/cookie.controller');
+const cookiesController = require("./controllers/cookie.controller");
 
 app.use(cookieParser()); // provides us access to the middleware so that we can see what cookies are stored.
 
-app.use('/', cookiesController);
+app.use("/", cookiesController);
 
 app.listen(PORT, log(`Running Cookies on Port: ${PORT}`));
